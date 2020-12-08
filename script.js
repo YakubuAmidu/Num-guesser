a40a2d546e881b77d060c515085e8fb8e7928b07
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-08 04:03:13 -0800

    Commiting changes

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+document.querySelector(".message").textContent="🎉 Correct Number";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
