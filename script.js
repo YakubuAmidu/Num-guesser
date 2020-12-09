34ed38badcfd7833eaa3b9b5fa04fbec1695b3c5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-09 06:21:36 -0800

    Commiting changes

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ console.log(document.querySelector(".guess").value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+let hightScore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -31,6 +33,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > hightScore) {
+      hightScore = score;
+    }
 
     // When player is too high
   } else if (guess > secretNumber) {
@@ -64,7 +70,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.guess').textContent = null;
 
-  document.querySelector('.score').textContent = '0';
+  document.querySelector('.score').textContent = score;
 
   document.querySelector('.number').textContent = '?';
 
