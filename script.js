4487d6cea3e6a32adac59f3a2a695acf47205cce
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-09 02:45:18 -0800

    Commiting changes

--- script.js
+++ script.js
@@ -25,9 +25,14 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You loose the game';
+      document.querySelector('.message').textContent = 0;
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = '📉 Too low';
     score--;
